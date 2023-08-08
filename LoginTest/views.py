ac861a9dcc6449c7d8098522ff8df91b09a50331
Speed <zhaoyi@cnsibo.com>
2023-08-08 19:28:17 +0800

    Ver1

--- LoginTest/views.py
+++ LoginTest/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import UserInfo
 
 
+class SignView(APIView):
+
+    def post(self, request, *args, **kwargs):
+
+        s = request.data
+        print(s["name"])
+        username = UserInfo.objects.filter(UserName=s["name"])
+        if not username.exists():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        User = UserInfo.objects.create(UserName=s["name"], PassWord=s["password"], PhoneNumber=s["phone"], )
+        return Response(data=request.data, status=status.HTTP_200_OK)
 
--- Login_Test/settings.py
+++ Login_Test/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'LoginTest.apps.LogintestConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
